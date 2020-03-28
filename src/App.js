f7e699b7f7a5a5d4fac8634bd0236aac9ae2ba91
Greg Hawthorne <ghawthorne@me.com>
2020-03-28 08:15:44 -0700

    add scss

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Monopoly from "./Monopoly/Monopoly"
+
+
+function App() {
+  return (
+    <div className="App">
+    <Monopoly />
+      
+    </div>
+  );
+}
+
+export default App;
